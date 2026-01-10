1323488579ea7aa251e6945b14cb0a1438589ed1
Danijela Medic <medic.danijeladaca@gmail.com>
2026-01-10 19:02:58 +0100

    Initial Spring Boot backend project

---
+++ src/main/java/jutjubic/isa/backend/JutjubicBackendApplication.java
@@ -0,0 +1,13 @@
+package jutjubic.isa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JutjubicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JutjubicBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jutjubic/isa/backend/JutjubicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jutjubic.isa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JutjubicBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
