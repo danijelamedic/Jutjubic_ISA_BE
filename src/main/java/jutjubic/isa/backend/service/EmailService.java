1ebf5f0f7c46328eed870156b90671a849e22b88
Danijela Medic <medic.danijeladaca@gmail.com>
2026-01-21 16:39:46 +0100

    fix: async email sender

--- src/main/java/jutjubic/isa/backend/service/EmailService.java
+++ src/main/java/jutjubic/isa/backend/service/EmailService.java
@@ -2,6 +2,7 @@ package jutjubic.isa.backend.service;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,13 +14,19 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    @Async("mailExecutor")
     public void sendActivationEmail(String to, String activationLink) {
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom("slike.rimini2024@gmail.com");
-        msg.setTo(to);
-        msg.setSubject("Aktivacija naloga");
-        msg.setText("Klikni na link za aktivaciju:\n" + activationLink);
+        try {
+            SimpleMailMessage msg = new SimpleMailMessage();
+            msg.setFrom("slike.rimini2024@gmail.com");
+            msg.setTo(to);
+            msg.setSubject("Aktivacija naloga");
+            msg.setText("Klikni na link za aktivaciju:\n" + activationLink);
 
-        mailSender.send(msg);
+            mailSender.send(msg);
+        } catch (Exception e) {
+            // Bitno: async slanje ne sme da obori registraciju
+            e.printStackTrace();
+        }
     }
 }
